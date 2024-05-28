95a494f90409c4ac70641610d02e775c80ff0428
varunification <letter2varun@gmx.com>
2024-05-28 19:07:08 +0530

    fix resposive flaw in contact us

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -125,7 +125,7 @@ export default function Contact() {
                                         d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                                     />
                                 </svg>
-                                <div className="ml-4 text-md tracking-wide font-semibold w-40">
+                                <div className="ml-4 text-md tracking-wide font-semibold w-40 overflow-wrap-break">
                                    salonnailsswanky@gmail.com
                                 </div>
                             </div>
