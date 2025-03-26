c4f6bad9b01ae5271072bf193885022c66ff7537
Varun Aggarwal <letter2varun@gmx.com>
2025-03-26 14:02:41 +0530

    some changes

--- src/components/Product/Item.jsx
+++ src/components/Product/Item.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Item = ({ prod }) => {
   return (
-    <div className="flex flex-col items-center mt-3 ml-2">
-      <img className="sm:w-96 w-96 mb-2" src={prod.image} alt="image2" />
+    <div className="flex flex-col items-center mt-3 ml-2 ">
+      <img className="sm:w-96 w-96 mb-2 h-[20rem]" src={prod.image} alt="image2" />
       <span className="ml-4 mb-8">{prod.title}</span>
     </div>
   );
